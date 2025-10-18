87e511dd1f058e2b9fe35142cf070734006392b2
JesusBonilla9 <jesus_bonilla@ucne.edu.do>
2025-10-18 15:42:12 -0400

    Se agregaron los filesScopes para los names spaces de los controllers ademas de terminar ambos controllers

--- GestionHuacales.Api9/Controllers/EntradasHuacalesController.cs
+++ GestionHuacales.Api9/Controllers/EntradasHuacalesController.cs
@@ -5,68 +5,67 @@ using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace GestionHuacales.Api9.Controllers
+namespace GestionHuacales.Api9.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class EntradasHuacalesController(EntradasHuacalesServices entradaHuacalesServices): ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class EntradasHuacalesController(EntradasHuacalesServices entradaHuacalesServices): ControllerBase
+    // GET: api/<EntradasHuacalesController>
+    [HttpGet]
+    public async Task<EntradasHuacalesDTO[]> Get()
     {
-        // GET: api/<EntradasHuacalesController>
-        [HttpGet]
-        public async Task<EntradasHuacalesDTO[]> Get()
-        {
-            return await entradaHuacalesServices.Listar(l => true);
-        }
+        return await entradaHuacalesServices.Listar(l => true);
+    }
 
-        // GET api/<EntradasHuacalesController>/5
-        [HttpGet("{id}")]
-        public async Task<EntradasHuacalesDTO[]>Get(int id)
-        {
-            return await entradaHuacalesServices.Listar(l => l.EntradaId == id);
-        }
+    // GET api/<EntradasHuacalesController>/5
+    [HttpGet("{id}")]
+    public async Task<EntradasHuacalesDTO[]>Get(int id)
+    {
+        return await entradaHuacalesServices.Listar(l => l.EntradaId == id);
+    }
 
-        // POST api/<EntradasHuacalesController>
-        [HttpPost]
-        public async Task Post([FromBody] EntradasHuacalesDTO entradahuacal)
+    // POST api/<EntradasHuacalesController>
+    [HttpPost]
+    public async Task Post([FromBody] EntradasHuacalesDTO entradahuacal)
+    {
+        var entrada = new EntradasHuacales
         {
-            var entrada = new EntradasHuacales
+            Fecha = DateTime.Now,
+            NombreCliente = entradahuacal.NombreCliente,
+            EntradasHuacalesDetalles = entradahuacal.Huacales.Select(h => new EntradasHuacalesDetalles
             {
-                Fecha = DateTime.Now,
-                NombreCliente = entradahuacal.NombreCliente,
-                EntradasHuacalesDetalles = entradahuacal.Huacales.Select(h => new EntradasHuacalesDetalles
-                {
-                    TipoId = h.TipoId,
-                    Cantidad = h.Cantidad,
-                    Precio = h.Precio,
-                }).ToArray()
-            };
-            await entradaHuacalesServices.Guardar(entrada);
-        }
+                TipoId = h.TipoId,
+                Cantidad = h.Cantidad,
+                Precio = h.Precio,
+            }).ToArray()
+        };
+        await entradaHuacalesServices.Guardar(entrada);
+    }
 
-        // PUT api/<EntradasHuacalesController>/5
-        [HttpPut("{id}")]
-        public async Task Put(int id, [FromBody] EntradasHuacalesDTO entradahuacal)
+    // PUT api/<EntradasHuacalesController>/5
+    [HttpPut("{id}")]
+    public async Task Put(int id, [FromBody] EntradasHuacalesDTO entradahuacal)
+    {
+        var entrada = new EntradasHuacales
         {
-            var entrada = new EntradasHuacales
+            EntradaId = id,
+            Fecha = DateTime.Now,
+            NombreCliente = entradahuacal.NombreCliente,
+            EntradasHuacalesDetalles = entradahuacal.Huacales.Select(h => new EntradasHuacalesDetalles
             {
-                EntradaId = id,
-                Fecha = DateTime.Now,
-                NombreCliente = entradahuacal.NombreCliente,
-                EntradasHuacalesDetalles = entradahuacal.Huacales.Select(h => new EntradasHuacalesDetalles
-                {
-                    TipoId = h.TipoId,
-                    Cantidad = h.Cantidad,
-                    Precio = h.Precio,
-                }).ToList()
-            };
-            await entradaHuacalesServices.Guardar(entrada);
-        }
+                TipoId = h.TipoId,
+                Cantidad = h.Cantidad,
+                Precio = h.Precio,
+            }).ToList()
+        };
+        await entradaHuacalesServices.Guardar(entrada);
+    }
 
-        // DELETE api/<EntradasHuacalesController>/5
-        [HttpDelete("{id}")]
-        public async Task Delete(int id)
-        {
-            await entradaHuacalesServices.Eliminar(id);
-        }
+    // DELETE api/<EntradasHuacalesController>/5
+    [HttpDelete("{id}")]
+    public async Task Delete(int id)
+    {
+        await entradaHuacalesServices.Eliminar(id);
     }
 }
--- GestionHuacales.Api9/Controllers/TiposHuacalesController.cs
+++ GestionHuacales.Api9/Controllers/TiposHuacalesController.cs
@@ -4,24 +4,23 @@ using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace GestionHuacales.Api9.Controllers
+namespace GestionHuacales.Api9.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TiposHuacalesController(EntradasHuacalesServices entradaHuacalesServices) : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class TiposHuacalesController(EntradasHuacalesServices entradaHuacalesServices) : ControllerBase
+    // GET: api/<TiposHuacalesController>
+    [HttpGet]
+    public async Task<TiposHuacalesDTO[]> Get()
     {
-        // GET: api/<TiposHuacalesController>
-        [HttpGet]
-        public async Task<TiposHuacalesDTO[]> Get()
-        {
-            return await entradaHuacalesServices.ListarTipos(t => true);
-        }
-        
-        // GET api/<TiposHuacalesController>/5
-        [HttpGet("{id}")]
-        public async Task<TiposHuacalesDTO[]> Get(int id)
-        {
-            return await entradaHuacalesServices.ListarTipos(t => t.TipoId == id);
-        }
+        return await entradaHuacalesServices.ListarTipos(t => true);
+    }
+    
+    // GET api/<TiposHuacalesController>/5
+    [HttpGet("{id}")]
+    public async Task<TiposHuacalesDTO[]> Get(int id)
+    {
+        return await entradaHuacalesServices.ListarTipos(t => t.TipoId == id);
     }
 }
